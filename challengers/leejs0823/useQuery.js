2d12391185e4a6181976e614dedd69077732a5ef
jungsun <dlwjdtjs0111@gmail.com>
2025-03-06 18:24:01 +0900

    수정

--- challengers/leejs0823/useQuery.js
+++ challengers/leejs0823/useQuery.js
@@ -8,11 +8,18 @@ function useQuery({ queryKey, queryFn, cacheTime }) {
   async function fetchData() {
     try {
       const cachedQuery = queryCache.getQuery(queryKey);
-      if (!cachedQuery || Date.now() - cachedQuery.timestamp > cacheTime) {
+      if (
+        !cachedQuery ||
+        Date.now() > cachedQuery.timestamp + cachedQuery.cacheTime
+      ) {
         // 해당 queryKey로 저장된 캐시가 없거나 저장된 데이터의 cacheTime이 만료되었을 경우 새로운 데이터를 가져온다
         const response = await queryFn();
         data = response;
-        queryCache.addQuery(queryKey, { data, timestamp: Date.now() });
+        queryCache.addQuery(queryKey, {
+          data,
+          timestamp: Date.now(),
+          cacheTime,
+        });
       } else {
         data = cachedQuery.data;
       }
